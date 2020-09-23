f80885033fbe5a4a93e83bcf4bab462019bf5f42
explr <explr@qq.com>
2020-09-23 11:56:39 +0000

    automatically update pages

--- main.py
+++
@@ -1,6 +0,0 @@
-from AcmeN import AcmeN
-from DnsHandlers import GoDaddyDNSHandler
-
-acme = AcmeN('account.key')
-dns = GoDaddyDNSHandler('sso-key *****:****')
-acme.get_cert_from_domain('foo.example.com', dns_name=['foo1.example.com', 'foo2.example.com'], cert_type='rsa', dns_handler=dns)
