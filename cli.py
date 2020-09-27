a94026dec10dd3f7ac13082d95e8cf9e8d2f1999
explr <explr@qq.com>
2020-09-27 22:04:45 +0800

    transform cli into a function, add arguments
    add the remaining arguments of revoke_cert and rotate_key
    add deactivate_account subcommand
    delete unused imports
    fix the 'dest' parameter of subparsers

--- cli.py
+++ cli.py
@@ -1,18 +1,19 @@
 import argparse
-from AcmeN import AcmeN
-import DnsHandlers
-import json
+import sys
 
-if __name__ == '__main__':
-    command = ''
+import DnsHandlers
+
+
+def cli(args):
     parser = argparse.ArgumentParser(description='A RFC8555 acme client.')
     parser.add_argument('--key', metavar='KEY_FILE', default='account.key',
                         help='the private key of the account, default:%(default)s')
 
-    subparsers = parser.add_subparsers(dest=command, required=True, metavar='command')
+    subparsers = parser.add_subparsers(dest='command', required=True, metavar='command')
     get_cert = subparsers.add_parser('getcert', help='get a certificate from acme server')
     revoke_cert = subparsers.add_parser('revokecert', help='revoke a certificate')
     rotate_key = subparsers.add_parser('rotatekey', help='change the key of the account')
+    deactivate_account = subparsers.add_parser('deactivate', help='DEACTIVATE current account, this is UNDOABLE')
 
     # get_cert command
     dns_providers = DnsHandlers.__all__[:]
@@ -26,3 +27,27 @@ if __name__ == '__main__':
                           metavar='dns_handler', help='the dns provider api handler, can be one of: %(choices)s')
     get_cert.add_argument('dns_param', nargs='*', metavar='dns_api_params',
                           help='optional, parameters for the DNSHandler')
+
+    # revoke_cert command
+    revoke_cert.add_argument('cert', metavar='CERT_PATH', help='the path to the certificate you want to revoke')
+    revoke_cert.add_argument('-p', '--pri-key', metavar='PRIVATE_KEY', default=argparse.SUPPRESS,
+                             help='the private key of the certificate')
+    revoke_cert.add_argument('-d', '--dns', choices=dns_providers, default='DefaultDNSHandler',
+                          metavar='dns_handler', help='the dns provider api handler, can be one of: %(choices)s')
+    revoke_cert.add_argument('dns_param', nargs='*', metavar='dns_api_params',
+                          help='optional, parameters for the DNSHandler')
+
+    # rotate_key command
+    rotate_key.add_argument('newkey', metavar='NEW_ACCOUNT_KEY',
+                            help='path to the new account key file')
+
+    result, dns_params = parser.parse_known_args(args)
+    result = vars(result)
+    # TODO: --key and -p are mutually exclusive.
+    # TODO: --dns should store a instance of DNSHandler initiated with command line params.
+    return vars(result)
+
+
+if __name__ == '__main__':
+    # TODO: invoke AcmeN functions
+    pass
